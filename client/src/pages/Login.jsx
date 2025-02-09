63dbcf13f2c0f503f8f763158a7a044492382637
Rishit Srivastava <rishitsrivastava43@gmail.com>
2025-02-10 05:00:55 +0530

    added toggle between sign up and sign in page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,7 +6,7 @@ export default function Login() {
   const [isLogin, setIsLogin] = useState(true)
 
   const toggleLogin = () => {
-    setIsLogin(false);
+    setIsLogin((prev) => !prev);
   }
   
   return (
@@ -29,7 +29,24 @@ export default function Login() {
                 <Button sx={{marginTop: "1rem"}} variant="text" fullWidth type="submit" onClick={toggleLogin}>Sign Up Instead?</Button>
               </form>
             </>
-          ) : <span>Register</span>
+          ) : <>
+            <Typography variant="h5">Sign Up</Typography>
+            <form style={{ width: "100%", marginTop: "1rem"}}>
+              <TextField required fullWidth label="Name" margin="normal" variant="outlined" />
+
+              <TextField required fullWidth label="Username" margin="normal" variant="outlined" />
+
+              <TextField required fullWidth label="Bio" margin="normal" variant="outlined" />
+
+              <TextField required fullWidth label="Passowrd" margin="normal" variant="outlined" />
+
+              <Button sx={{marginTop: "1rem"}} variant="contained" fullWidth color="primary" type="submit">Sign Up</Button>
+
+              <Typography sx={{display:"flex", paddingTop: 4, flexDirection:"column", alignItems:"center" }}>Or</Typography>
+              
+              <Button sx={{marginTop: "1rem"}} variant="text" fullWidth type="submit" onClick={toggleLogin}>Have Account? Login</Button>
+            </form>
+        </>
           
         }
       </Paper>

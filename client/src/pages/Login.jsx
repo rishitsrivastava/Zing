581c59473bf390c78976965a099f3bf504936ebd
Rishit Srivastava <rishitsrivastava43@gmail.com>
2025-02-10 04:54:11 +0530

    set up the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,20 +10,23 @@ export default function Login() {
   }
   
   return (
-    <Container component={"main"} maxWidth="xs">
+    <Container component={"main"} maxWidth="xs" sx={{height: "100vh", display: "flex", justifyContent: "center" , alignItems: "center"}}>
       <Paper elevation={4} sx={{padding: 4, display: "flex", flexDirection:"column", alignItems:"center"}}>
 
         {
           isLogin ? (
             <>
               <Typography variant="h5">Login</Typography>
-              <form>
+              <form style={{ width: "100%", marginTop: "1rem"}}>
                 <TextField required fullWidth label="Username" margin="normal" variant="outlined" />
+
                 <TextField required fullWidth label="Passowrd" margin="normal" variant="outlined" />
 
                 <Button sx={{marginTop: "1rem"}} variant="contained" fullWidth color="primary" type="submit">Login</Button>
+
                 <Typography sx={{display:"flex", paddingTop: 4, flexDirection:"column", alignItems:"center" }}>Or</Typography>
-                <Button sx={{marginTop: "1rem"}} variant="text" fullWidth type="submit" onClick={toggleLogin}>Register</Button>
+                
+                <Button sx={{marginTop: "1rem"}} variant="text" fullWidth type="submit" onClick={toggleLogin}>Sign Up Instead?</Button>
               </form>
             </>
           ) : <span>Register</span>
